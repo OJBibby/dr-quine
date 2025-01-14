189ea4a292d538c25b6b4abbdb3a24c74b202a69
Oliver Bibby <obibby@c2r11s3.42wolfsburg.de>
2025-01-14 20:30:28 +0100

    more

--- Colleen.c
+++ Colleen.c
@@ -6,38 +6,68 @@
 /*   By: obibby <obibby@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 17:57:51 by obibby            #+#    #+#             */
-/*   Updated: 2025/01/13 18:28:08 by obibby           ###   ########.fr       */
+/*   Updated: 2025/01/14 20:29:54 by obibby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void blah()
+#include <stdio.h>
+
+void blah(char *str)
 {
-    printf("");
+    putchar(' '); putchar(' '); putchar(' '); putchar(' ');
+    putchar('\"');
+    for (; *str; str++) {
+        if (*str == '\n') {
+            putchar('\\'); putchar('n');
+        } else if (*str == '\\') {
+            putchar('\\'); putchar('\\');
+        } else if (*str == '\"') {
+            putchar('\\'); putchar('\"');
+        } else
+            putchar(*str);
+    }
+    putchar('\"'); putchar(','); putchar('\n');
 }
 
 int main()
 {
-    char *a[] = {
-    "/* ************************************************************************** */"
-    "/*                                                                            */"
-    "/*                                                        :::      ::::::::   */"
-    "/*   Colleen.c                                          :+:      :+:    :+:   */"
-    "/*                                                    +:+ +:+         +:+     */"
-    "/*   By: obibby <obibby@student.42wolfsburg.de>     +#+  +:+       +#+        */"
-    "/*                                                +#+#+#+#+#+   +#+           */"
-    "/*   Created: 2025/01/13 17:57:51 by obibby            #+#    #+#             */"
-    "/*   Updated: 2025/01/13 18:08:17 by obibby           ###   ########.fr       */"
-    "/*                                                                            */"
-    "/* ************************************************************************** */"
-
-    "void blah()\n"
-    "{\n"
-    "   printf(\"\");"
-    "}\n"
-    "int main()\n"
-    "   char *a[] = {\n",
-    };
-    char *b[] = {"\\"};
     // This is another comment
-    blah();
+    char *a[] = {
+    "/* ************************************************************************** */\n",
+    "/*                                                                            */\n",
+    "/*                                                        :::      ::::::::   */\n",
+    "/*   Colleen.c                                          :+:      :+:    :+:   */\n",
+    "/*                                                    +:+ +:+         +:+     */\n",
+    "/*   By: obibby <obibby@student.42wolfsburg.de>     +#+  +:+       +#+        */\n",
+    "/*                                                +#+#+#+#+#+   +#+           */\n",
+    "/*   Created: 2025/01/13 17:57:51 by obibby            #+#    #+#             */\n",
+    "/*   Updated: 2025/01/13 18:08:17 by obibby           ###   ########.fr       */\n",
+    "/*                                                                            */\n",
+    "/* ************************************************************************** */\n",
+    "\n",
+    "#include <stdio.h>\n",
+    "\n",
+    "void blah()\n",
+    "{\n",
+    "   printf(\"\");",
+    "}\n",
+    "\n",
+    "int main()\n",
+    "{\n",
+    "    // This is another comment\n",
+    "    char *a[] = {\n",
+    NULL
+    };
+    char *b[] = {
+        "};\n"
+        "char *b[] = {",
+        NULL
+    }, **x;
+    for (x = a; *x; x++) {
+        printf("%s", *x);
+    }
+    for (x = a; *x; x++) {
+        blah(*x);
+    }
+    blah(*b);
 }
